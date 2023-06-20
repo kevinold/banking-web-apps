a09eb680ee56bf71763793570566084f5fed00c9
Kevin Old <kevin@kevinold.com>
2023-06-20 11:22:02 -0500

    fix mortgagecalc component test and install aws-amplify package required for Amplify UI

--- packages/shared/ui/src/lib/mortgageCalc.spec.tsx
+++ packages/shared/ui/src/lib/mortgageCalc.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import SharedUi from './shared-ui';
+import MortgageCalc from './mortgageCalc';
 
-describe('SharedUi', () => {
+describe('MortgageCalc', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<SharedUi />);
+    const { baseElement } = render(<MortgageCalc />);
     expect(baseElement).toBeTruthy();
   });
 });
